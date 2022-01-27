ed90a81a11501ffc88370b06d1b730ed65a28dc7
cagena <98061439+cagena@users.noreply.github.com>
2022-01-27 09:12:19 -0800

    Lab 2 1/27 9:12am
    Added all the source code.

---
+++ controller_agena_chiu.py
@@ -0,0 +1,55 @@
+'''!
+@file controller_agena_chiu.py
+This is the file that serves as a module to be imported in main. It creates a class for the controller
+driver to run functions.
+@author Corey Agena
+@author Luisa Chiu
+@date 1-27-2022
+'''
+
+import pyb
+    
+class ControllerDriver:  
+    '''! 
+    This class implements a controller driver for a motor and flywheel rig. 
+    '''
+    def __init__(self, K_p, i_set):
+        '''! 
+        Creates a controller driver by initializing the gain
+        and initial setpoint used in closed-loop control.
+        @param K_p The initial proportional gain for the controller.
+        @param i_set The initial setpoint for the controller.
+        '''
+        ## Proportional Gain
+        self.K_p = K_p
+        
+        ## Initial setpoint
+        self.i_set = i_set
+        
+        print ('Creating a controller driver')
+        
+    def run(self,pos):
+        '''!
+        Called repeatedly to run the control algorithm.
+        @param pos Current position of the encoder.
+        @return duty Duty cycle to set the motor.
+        '''
+        ## Error, difference between the current position and initial setpoint.
+        error = pos - self.i_set
+        ## Actuation signal or percent duty cycle to set the motor.
+        duty = error*self.K_p
+        return duty
+        
+    def set_setpoint(self,setpoint):
+        '''!
+        Sets the setpoint.
+        @param setpoint The new setpoint used for the controller.
+        '''
+        self.i_set = setpoint
+        
+    def set_gain(self, gain):
+        '''!
+        Sets the proportional gain.
+        @param gain The new proportional gain used for the controller.
+        '''
+        self.K_p = gain
---
+++ main.py
@@ -0,0 +1,33 @@
+'''!
+@file main.py
+This is the main file for Lab 1, where two motor driver and encoder driver objects are created to run functions,
+such as setting duty cycles for the motors and printing encoder position.
+@author Corey Agena
+@author Luisa Chiu
+@date 1-26-2022
+'''
+
+# Import the required modules to run the motor and encoder 
+import motor_agena_chiu
+import encoder_agena_chiu
+import pyb
+
+if __name__ == '__main__':
+    ## A variable that creates a encoder driver for encoder 1.
+    encoder_drv1 = encoder_agena_chiu.EncoderDriver(pyb.Pin.cpu.B6, pyb.Pin.cpu.B7, 4)
+    ## A variable that creates a encoder driver for encoder 2.
+    encoder_drv2 = encoder_agena_chiu.EncoderDriver(pyb.Pin.cpu.C6, pyb.Pin.cpu.C7, 8)
+    ## A variable that creates a motor driver for motor 1.
+    motor_drv1 = motor_agena_chiu.MotorDriver(pyb.Pin.cpu.A10, pyb.Pin.cpu.B4, pyb.Pin.cpu.B5, 3)
+    ## A variable that creates a motor driver for motor 2.
+    motor_drv2 = motor_agena_chiu.MotorDriver(pyb.Pin.cpu.C1, pyb.Pin.cpu.A0, pyb.Pin.cpu.A1, 5)
+    # Enable both motors.
+    motor_drv1.enable()
+    motor_drv2.enable()
+    # Set the duty cycle for both motors.
+    motor_drv1.set_duty_cycle(50)
+    motor_drv2.set_duty_cycle(50)
+    # While loop to continuously print the position of both encoders.
+    while True:
+        print('Encoder 1 position: ' + str(encoder_drv1.read()))
+        print('Encoder 2 position: ' + str(encoder_drv2.read()))
