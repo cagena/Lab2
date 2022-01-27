fbbc97a87e6a1ffb277290f6fd9acb76f48d81fa
cagena <98061439+cagena@users.noreply.github.com>
2022-01-27 10:34:42 -0800

    plot file

--- plot.py
+++ plot.py
@@ -1,23 +1,29 @@
+# Import modules required to plot.
 from matplotlib import pyplot
+import serial
 
-filename = 'eric.csv'
+# Create empty lists.
 x_val = []
 y_val = []
 
-with open(filename, 'r') as f:
+# Open the serial port to read from it and plot.
+with serial.Serial('COMx', 115200) as f:
     while True:
         raw_data = f.readline()
 #       Next line obtained from: https://stackoverflow.com/questions/53919299/python-how-to-properly-use-readline-and-readlines
         if not raw_data: break
-        data = raw_data.split(',')
-        if data[0].strip() == '' or data[1].strip() == '':
+        data = raw_data.split(b',')
+        if data[0].strip() == b'' or data[1].strip() == b'':
             continue
         elif data[0].strip().isalpha() == True or data[1].strip().isalpha() == True:
              continue
         else:
             x_val.append(float(data[0].strip()))
-            y_val.append(float(data[1].strip())) 
+            y_val.append(float(data[1].strip()))
+    # Plot the values read from the serial port.
     pyplot.plot(x_val, y_val)
-    pyplot.xlabel('x-axis')
-    pyplot.ylabel('y-axis')
+    # Label both axes.
+    pyplot.xlabel('time [ms]')
+    pyplot.ylabel('position [ticks]')
+    # Show the plot.
     pyplot.show()
