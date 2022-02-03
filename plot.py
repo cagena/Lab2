24b5309a4a096799d4b304b796c838b5508672da
cagena <98061439+cagena@users.noreply.github.com>
2022-02-02 16:01:10 -0800

    Lab2 2/2 4:01

--- plot.py
+++ plot.py
@@ -16,21 +16,22 @@ y_val = []
 with serial.Serial('COM21', 115200) as f:
     f.write(b'\x03')
     f.write(b'\x04')
-    if f.readline(-1) == b'raw REPL; CTRL-B to exit\r\n':
+    if b'CTRL-B' in f.readline(-1):
         f.write(b'\x02')
         f.write(b'\x04')
+    time.sleep(0.05)
+    f.write(b'0.15\r\n')
+#     K_p = input('Input Kp to run step response, input s to stop: ')
+#     f.write(bytes('{:}\r\n'.format(K_p), 'utf8'))
     time.sleep(0.25)
-    f.write(b'0.1\r\n')
-    time.sleep(1)
     f.write(b'16384\r\n')
-    time.sleep(1.25)
+    time.sleep(0.25)
     while True:
         raw_data = f.readline()
 #       Next line obtained from: https://stackoverflow.com/questions/53919299/python-how-to-properly-use-readline-and-readlines
 #         if not raw_data:
 #             break
         data = raw_data.split(b',')
-        print(raw_data)
         try:
             float(data[0])
             float(data[1])
@@ -47,7 +48,6 @@ with serial.Serial('COM21', 115200) as f:
         else:
             x_val.append(float(data[0].strip()))
             y_val.append(float(data[1].strip()))
-    print('hi')
     # Plot the values read from the serial port.
     pyplot.plot(x_val, y_val)
     #pyplot.plot(list1,list2)
